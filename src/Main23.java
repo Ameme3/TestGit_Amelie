41c19844b8a29f94039c3ca9d9ad7e3d2e4c1fe8
oppik <oppik@10.91.129.24>
2018-03-17 12:51:37 +0100

    troll!

---
+++ src/Main23.java
@@ -0,0 +1,12 @@
+import ch.dsl.bachelor.git.TestGit;
+
+public class Main23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TestGit myFirstTestGit = new TestGitUpdate("My parameter");
+		
+	}
+
+}
