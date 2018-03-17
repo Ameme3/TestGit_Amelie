beef9bdd21cebca98f1823fe862771700191d3af
Amélie <Amlie@10.91.129.126>
2018-03-17 12:58:56 +0100

    Owner Parameter

--- src/ch/dsl/bachelor/git/Main.java
+++ src/ch/dsl/bachelor/git/Main.java
@@ -3,7 +3,7 @@ package ch.dsl.bachelor.git;
 public class Main {
 
 	public static void main(String[] args) {
-		TestGit myFirstTestGit = new TestGitUpdate("MY Parameter");
+		TestGit myFirstTestGit = new TestGitUpdate("Owner Parameter");
 		System.out.println(myFirstTestGit.returnTheString());
 
 	}
