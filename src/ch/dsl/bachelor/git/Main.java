bf59ac5f033868ccb3b7ba9bbfbb7339f7c1514c
Alex <alex.olivieri81@gmail.com>
2018-03-17 12:50:57 +0100

    Developer Changed Something

---
+++ src/ch/dsl/bachelor/git/Main.java
@@ -0,0 +1,11 @@
+package ch.dsl.bachelor.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TestGit myFirstTestGit = new TestGitUpdate("MY Parameter");
+		System.out.println(myFirstTestGit.returnTheString());
+
+	}
+
+}
---
+++ src/ch/dsl/bachelor/git/TestGitUpdate.java
@@ -0,0 +1,10 @@
+package ch.dsl.bachelor.git;
+
+public class TestGitUpdate extends TestGit {
+
+	public TestGitUpdate(String parameter) {
+		super(parameter);
+		// TODO Auto-generated constructor stub
+	}
+
+}
